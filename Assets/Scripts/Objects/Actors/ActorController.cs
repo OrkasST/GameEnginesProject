7998317ba4da479c37b7eda7dbcbc89082edff54
Егор Каскевич <->
2026-06-26 21:07:41 +0300

    Actor State Machine implemented

--- Assets/Scripts/Objects/Actors/ActorController.cs
+++ Assets/Scripts/Objects/Actors/ActorController.cs
@@ -4,6 +4,8 @@ namespace Assets.Scripts.Objects.Actors
 {
     public class ActorController : MonoBehaviour
     {
-        
+        public ActorStateMachine StateMachine { get; private set; } = new();
+
+
     }
 }
--- Assets/Scripts/Objects/Actors/ActorStateMachine.cs
+++ Assets/Scripts/Objects/Actors/ActorStateMachine.cs
@@ -1,10 +1,66 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Assets.Scripts.Objects.Actors
 {
-    [RequireComponent(typeof(ActorController))]
+    public enum ActorState { Running, Standing, Jumping, Falling, Attacking, TakingDamage, Interacting, Dashing }
+    public enum LookDirection { Left, Right, None }
+
     public class ActorStateMachine
     {
+        private List<ActorState> _futureStates = new();
+        private LookDirection _registeredDirection;
 
+        public ActorState CurrentState { get; private set; }
+        public ActorState LastState { get; private set; }
+
+        public LookDirection CurrentDirection { get; private set; }
+        public LookDirection LastDirection { get; private set; }
+
+
+        public void RegisterStateChange(ActorState state)
+        {
+            _futureStates.Add(state);
+        }
+        public void RegisterStateChange(LookDirection lookDirection)
+        {
+            _registeredDirection = lookDirection;
+        }
+        public void UpdateState()
+        {
+            for (int i = _futureStates.Count - 1; i >= 0; i--)
+            {
+                if (CanBeApplied(_futureStates[i])) ChangeState(_futureStates[i]);
+                if (CanBeApplied(_registeredDirection)) ChangeDirection(_registeredDirection);
+            }
+        }
+
+        private bool CanBeApplied(ActorState actorState)
+        {
+            if (CurrentState == ActorState.TakingDamage && actorState != ActorState.Standing) return false;
+            if (CurrentState == ActorState.Jumping && actorState != ActorState.Falling) return false;
+            if (CurrentState == ActorState.Falling)
+            {
+                if (actorState != ActorState.Standing || actorState != ActorState.Running) return false;
+            }
+            return true;
+        }
+        private bool CanBeApplied(LookDirection direction)
+        {
+            if (CurrentState == ActorState.Dashing) return false;
+            return true;
+        }
+
+        private void ChangeState(ActorState state)
+        {
+            LastState = CurrentState;
+            CurrentState = state;
+        }
+        private void ChangeDirection(LookDirection lookDirection)
+        {
+            LastDirection = lookDirection;
+            CurrentDirection = lookDirection;
+            _registeredDirection = LookDirection.None;
+        }
     }
 }
